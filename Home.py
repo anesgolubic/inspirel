f6c97e701f97e30aedc007563b38182aba3963f3
anesgolubic <anes.golubic@gmail.com>
2024-03-10 21:04:15 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(
-    page_title="OLX V3 KPIs",
+    page_title="Inspirel",
     #page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
@@ -23,3 +23,45 @@ import openpyxl
 df = pd.read_csv('Inspirel_consolidated.csv')
 
 st.write(df)
+
+df['Datum'] = df['Datum'].str.replace(" ","")
+df['Datum'] = pd.to_datetime(df['Datum'], format='mixed', dayfirst=True)
+#Napraviti Entitet polje
+
+def set_color(row):
+    if row["Regija"] == "11 - Brčko Distrikt":
+        return "BD"
+    elif row["Regija"] == "12 - RS":
+        return "RS"
+    else:
+        return "FBiH"
+
+df = df.assign(Entitet=df.apply(set_color, axis=1))
+
+df['Year'] = df['Datum'].dt.year 
+df['Month'] = df['Datum'].dt.month 
+
+#Dataframovi
+
+#Ostvarena prodaja po mjesecu
+by_month = df.groupby(['Year','Month'])['Količina'].sum().reset_index()
+
+
+#Omjer prodaje po artikli
+by_product = df.groupby(['Artikal'])['Količina'].sum().reset_index()
+
+
+#Ostvarena prodaja po regionu
+by_region = df.groupby(['Entitet','Regija'])['Količina'].sum().reset_index()
+
+
+#Omjer prodaje po regionu
+by_entity = df.groupby(['Entitet'])['Količina'].sum().reset_index()
+
+
+#Ostvarena prodaja po mjesecu i artiklu
+by_month_product = df.groupby(['Year','Month','Artikal'])['Količina'].sum().reset_index()
+
+
+#Ostvarena prodaja po regionu i artiklu
+by_region_product = df.groupby(['Entitet','Regija','Artikal'])['Količina'].sum().reset_index()
