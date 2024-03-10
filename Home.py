2b4298226742df4a3bb1313fd3577afe142e7f1d
anesgolubic <anes.golubic@gmail.com>
2024-03-10 22:08:25 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -40,7 +40,7 @@ df = df.assign(Entitet=df.apply(set_color, axis=1))
 
 df['Year'] = df['Datum'].dt.year 
 df['Month'] = df['Datum'].dt.month 
-df['Short title'] = df['Artikač'].str.split(' ').str.[0]
+df['Short title'] = df['Artikal'].str.split(' ').str[0]
 
 
 #Filteri
