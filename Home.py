b0e38290ca3e61844d354df24f743afd57ab22bf
anesgolubic <anes.golubic@gmail.com>
2024-03-10 23:46:12 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -28,54 +28,40 @@ st.write(a)
 #Global variables
 ignore_sheets = ['Bonus 2023','Izgubljena prodaja Dorzol','Cumulative Dorzol','Bonus 2022','Weekly sales','Cumulative']
 
+@st.cache
+def load_data(file, ignore_sheets):
 
-def get_sheets(file, ignore_sheets):
     sheets = []
-
-    # Load the Excel workbook
     workbook = openpyxl.load_workbook(file)
-
-    # Get all sheet names
     sheet_names = workbook.sheetnames
 
-    # Print all sheet names
     for sheet_name in sheet_names:
         if sheet_name in ignore_sheets:
             pass
         else:
             sheets.append(sheet_name)
     
-    return sheets
-
-def get_data(file, sheet):
-    df = pd.read_excel(file, sheet_name=sheet)
-    
-    #Drop empty rows
-    df.dropna(subset=['Artikal'], inplace=True)
-    
-    # Extract values in the 'Artikal' column where other columns are NaN
-    new_column_values = df['Artikal'].where(df.drop(columns='Artikal').isnull().all(axis=1))
-
-    # Forward fill the extracted values
+    sheet = sheets[0]
+    df1 = pd.read_excel(file, sheet_name=sheet)
+    df1.dropna(subset=['Artikal'], inplace=True)
+    new_column_values = df1['Artikal'].where(df1.drop(columns='Artikal').isnull().all(axis=1))
     new_column_values.ffill(inplace=True)
+    df1['Regija'] = new_column_values
+    df1.dropna(subset=['Datum'], inplace=True)
 
-    # Add the new column to the DataFrame
-    df['Regija'] = new_column_values
-    
-    #Drop empty rows
-    df.dropna(subset=['Datum'], inplace=True)
-    
-    return df
+    for sheet in sheets[1:]:
+        st.write(sheet)
+        df = pd.read_excel(file, sheet_name=sheet)
+        df.dropna(subset=['Artikal'], inplace=True)
+        new_column_values = df['Artikal'].where(df.drop(columns='Artikal').isnull().all(axis=1))
+        new_column_values.ffill(inplace=True)
+        df['Regija'] = new_column_values
+        df.dropna(subset=['Datum'], inplace=True)
+        df1 = pd.concat([df, df1], axis=0)
 
-sheets = get_sheets(file, ignore_sheets)
+    return df1
 
-df1 = get_data(file, sheets[0])
-
-for sheet in sheets[1:]:
-    df = get_data(file, sheet)
-    df1 = pd.concat([df, df1], axis=0)
-
-df = df1
+df = load_data(file, ignore_sheets)
 
 
 #df = pd.read_csv('Inspirel_consolidated.csv')
