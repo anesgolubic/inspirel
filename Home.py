6c6b320b83f62dcb6ff8233fdfed7895b6d1ffb6
anesgolubic <anes.golubic@gmail.com>
2024-03-10 23:40:21 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,9 +21,64 @@ import gdown
 """
 url = "https://drive.google.com/drive/folders/1DjdfdT4yF9NAWaZgw4n7oVHGe_hCKnEk"
 a = gdown.download_folder(url, quiet=True, use_cookies=False)
+file = a[0]
 st.write(a)
 
-df = pd.read_csv('Inspirel_consolidated.csv')
+
+#Global variables
+ignore_sheets = ['Bonus 2023','Izgubljena prodaja Dorzol','Cumulative Dorzol','Bonus 2022','Weekly sales','Cumulative']
+
+
+def get_sheets(file, ignore_sheets):
+    sheets = []
+
+    # Load the Excel workbook
+    workbook = openpyxl.load_workbook(file)
+
+    # Get all sheet names
+    sheet_names = workbook.sheetnames
+
+    # Print all sheet names
+    for sheet_name in sheet_names:
+        if sheet_name in ignore_sheets:
+            pass
+        else:
+            sheets.append(sheet_name)
+    
+    return sheets
+
+def get_data(file, sheet):
+    df = pd.read_excel(file, sheet_name=sheet)
+    
+    #Drop empty rows
+    df.dropna(subset=['Artikal'], inplace=True)
+    
+    # Extract values in the 'Artikal' column where other columns are NaN
+    new_column_values = df['Artikal'].where(df.drop(columns='Artikal').isnull().all(axis=1))
+
+    # Forward fill the extracted values
+    new_column_values.ffill(inplace=True)
+
+    # Add the new column to the DataFrame
+    df['Regija'] = new_column_values
+    
+    #Drop empty rows
+    df.dropna(subset=['Datum'], inplace=True)
+    
+    return df
+
+sheets = get_sheets(file, ignore_sheets)
+
+df1 = get_data(file, sheets[0])
+
+for sheet in sheets[1:]:
+    df = get_data(file, sheet)
+    df1 = pd.concat([df, df1], axis=0)
+
+df = df1
+
+
+#df = pd.read_csv('Inspirel_consolidated.csv')
 
 df['Datum'] = df['Datum'].str.replace(" ","")
 df['Datum'] = pd.to_datetime(df['Datum'], format='mixed', dayfirst=True)
