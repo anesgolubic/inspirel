e91e7873194052ceb7cc1ba5e93da3e90e08aa77
anesgolubic <anes.golubic@gmail.com>
2024-04-04 23:42:10 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -126,7 +126,7 @@ def total_artikal(artikal):
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
     art = 'Dorzol'
-    st.metric(label=art,value=int(total_artikal(art)),delta=None)
+    st.metric(label=art,value=int(f"{total_artikal(art):,}"),delta=None)
 with col2:
     art = 'Glaumax'
     st.metric(label=art,value=int(total_artikal(art)),delta=None)
