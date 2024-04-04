2d29484ad7e29c889e2962b299c4c279908c01c1
anesgolubic <anes.golubic@gmail.com>
2024-04-05 00:45:08 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -279,3 +279,7 @@ st.plotly_chart(fig, use_container_width=True, config=dict(
 by_region_product_pivot = by_region_product.pivot(index=['Entitet','Regija'], columns='Short_title', values='Količina')
 by_region_product_pivot = by_region_product_pivot.sort_values(by=['Regija'])
 st.write(by_region_product_pivot)
+
+by_region_product_pivot2 = by_region_product.pivot(index='Short_title', columns=['Entitet','Regija'], values='Količina')
+by_region_product_pivot2 = by_region_product_pivot.sort_values(by=['Short_title'])
+st.write(by_region_product_pivot)
