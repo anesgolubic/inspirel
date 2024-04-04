196942a9c7f4260f1eedd4b9b5a935c51c7f46b5
anesgolubic <anes.golubic@gmail.com>
2024-04-05 00:26:53 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -229,9 +229,9 @@ by_month_product["Period"] = by_month_product["Year"].astype(str) +"/" + by_mont
 by_month_product = by_month_product.sort_values(by=['Year','Month','Short_title'], ascending=True).reset_index(drop=True)
 #by_month_product = by_month_product[['Period','Short_title','Količina']]
 #fig = px.bar(by_month_product, x='Period', y='Količina', color='Artikal', text_auto=True)
-by_month_product2 = by_month_product.pivot(index='Period',columns='Short_title',values='Količina')
+by_month_product2 = by_month_product.pivot(index='Period',columns='Short_title',values='Količina').fillna(0)
 st.write(by_month_product2)
-fig = px.line(by_month_product, x='Period', y='Količina', color='Short_title', color_discrete_map=color_map_artikli)
+fig = px.line(by_month_product2, x='Period', y=['Dorzol','Glaumax'], color='Short_title', color_discrete_map=color_map_artikli)
 fig.update_layout(dragmode=False)
 fig.update_layout(yaxis_title=None)
 fig.update_layout(xaxis_title=None)
