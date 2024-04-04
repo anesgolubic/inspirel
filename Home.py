74da8fed68825c88a36b8c3170845aec970ce0fa
anesgolubic <anes.golubic@gmail.com>
2024-04-04 23:52:19 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -157,6 +157,7 @@ with col1:
     fig.update_layout(xaxis_title=None)
     fig.update_xaxes(type='category')
     fig.update_xaxes(nticks=12) 
+    fig.update_layout(showlegend=False)
     #fig.update_traces(textposition='inside')
     st.plotly_chart(fig, use_container_width=True, config=dict(
         displayModeBar=False))
