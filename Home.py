2b467fb411ce67b34d9fd3fe1ed768ecef6f60a2
anesgolubic <anes.golubic@gmail.com>
2024-04-05 00:45:35 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -281,5 +281,5 @@ by_region_product_pivot = by_region_product_pivot.sort_values(by=['Regija'])
 st.write(by_region_product_pivot)
 
 by_region_product_pivot2 = by_region_product.pivot(index='Short_title', columns=['Entitet','Regija'], values='Količina')
-by_region_product_pivot2 = by_region_product_pivot.sort_values(by=['Short_title'])
+#by_region_product_pivot2 = by_region_product_pivot.sort_values(by=['Short_title'])
 st.write(by_region_product_pivot)
