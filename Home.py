c523a94fbc5b0c87d5df031354c0d09a6cbf8d86
anesgolubic <anes.golubic@gmail.com>
2024-04-05 00:14:12 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -226,6 +226,7 @@ with col2:
 #Ostvarena prodaja po mjesecu i artiklu
 by_month_product = df1.groupby(['Year','Month','Short_title'])['Količina'].sum().reset_index()
 by_month_product["Period"] = by_month_product["Year"].astype(str) +"/" + by_month_product["Month"].astype(str)
+by_month_product = by_month_product.sort_values(by='Period', ascending=True)
 #fig = px.bar(by_month_product, x='Period', y='Količina', color='Artikal', text_auto=True)
 fig = px.line(by_month_product, x='Period', y='Količina', color='Short_title', color_discrete_map=color_map_artikli)
 fig.update_layout(dragmode=False)
